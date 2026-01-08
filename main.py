a580653dcf6338a6b7c8290590c9303818372884
David Randall <davidra@andrews.edu>
2026-01-08 17:02:52 -0500

    Begin file setup

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+from PIL import Image
+
+SIZE = 1000
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <url/text>")
+        return
+    
+    url = sys.argv[1]
+    url_bytes = url.encode()
+    print(len(url_bytes))
+
+    bits = []
+    for byte in url_bytes:
+        for i in range(7, -1, -1):
+            bits.append((byte >> i) & 1)
+    
+    print(bits)
+    # PIL accesses images in Cartesian co-ordinates, so it is Image[columns, rows]
+    img = Image.new( 'RGB', (SIZE, SIZE), "black") # create a new black image
+    pixels = img.load() # create the pixel map
+
+    # for i in range(img.size[0]):    # for every col:
+    #     for j in range(img.size[1]):    # For every row
+    #         pixels[i, j] = (i//4, j//4, 100) # set the colour accordingly
+
+    img.show()
+
+if __name__ == "__main__":
+    main()
+
